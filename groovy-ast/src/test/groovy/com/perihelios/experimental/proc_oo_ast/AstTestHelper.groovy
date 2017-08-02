65336a0d15616eb23698b92b08875818f7067f5c
Jonathan Cottrill <bitbucket@jonathancottrill.net>
2017-08-02 15:42:10 -0500

    Remove unused code (service loader is working appropriately, now)

--- groovy-ast/src/test/groovy/com/perihelios/experimental/proc_oo_ast/AstTestHelper.groovy
+++ groovy-ast/src/test/groovy/com/perihelios/experimental/proc_oo_ast/AstTestHelper.groovy
@@ -1,30 +1,10 @@
 package com.perihelios.experimental.proc_oo_ast
 
-import org.codehaus.groovy.control.CompilationFailedException
-import org.codehaus.groovy.control.CompilationUnit
-import org.codehaus.groovy.control.CompilePhase
-import org.codehaus.groovy.control.CompilerConfiguration
-import org.codehaus.groovy.control.SourceUnit
-import org.codehaus.groovy.transform.ASTTransformation
-import org.codehaus.groovy.transform.GroovyASTTransformation
-
-import java.security.CodeSource
-
 class AstTestHelper {
 	private final GroovyClassLoader loader
 
-	AstTestHelper(Class<? extends ASTTransformation> transformationClass) {
-		GroovyASTTransformation annotation = transformationClass.getAnnotation(GroovyASTTransformation)
-
-		if (!annotation) {
-			throw new RuntimeException("$transformationClass.name is not annotated with " +
-				GroovyASTTransformation.name + "; cannot proceed")
-		}
-
-		CompilePhase compilePhase = annotation.phase()
-		ASTTransformation transformation = transformationClass.newInstance()
-
-		loader = new TransformingClassLoader(transformation, compilePhase)
+	AstTestHelper() {
+		loader = new GroovyClassLoader()
 	}
 
 	AstTestHelper parseCode(String groovyCode) {
@@ -35,35 +15,5 @@ class AstTestHelper {
 
 	Class<?> getClass(String name) {
 		return loader.loadClass(name)
-	}
-
-	private static class TransformingClassLoader extends GroovyClassLoader {
-		private final ASTTransformation transformation
-		private final CompilePhase compilePhase
-
-		TransformingClassLoader(ASTTransformation transformation, CompilePhase compilePhase) {
-			this.transformation = transformation
-			this.compilePhase = compilePhase
-		}
-
-		@Override
-		protected CompilationUnit createCompilationUnit(CompilerConfiguration config, CodeSource codeSource) {
-			CompilationUnit cu = super.createCompilationUnit(config, codeSource)
-//			cu.addPhaseOperation(new TransformingPhaseOperation(transformation), compilePhase.phaseNumber)
-			return cu
-		}
-	}
-
-	private static class TransformingPhaseOperation extends CompilationUnit.SourceUnitOperation {
-		private final ASTTransformation transformation
-
-		TransformingPhaseOperation(ASTTransformation transformation) {
-			this.transformation = transformation
-		}
-
-		@Override
-		void call(SourceUnit source) throws CompilationFailedException {
-			transformation.visit(null, source)
-		}
 	}
 }
